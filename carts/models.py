92edbadf7468e38aabff2dcbbc461bf296ca1a72
Mahmoud Gamal <mahmoudgamal19@gmail.com>
2021-10-26 01:39:20 +0200

    added pagination and search functionality

--- carts/models.py
+++ carts/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-from carts.views import cart
 
 from store.models import Product
 
@@ -20,6 +19,9 @@ class CartItem(models.Model):
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
     quantity = models.IntegerField()
     is_active = models.BooleanField(default=True)
+
+    def sub_total(self):
+        return self.product.price * self.quantity
 
     def __str__(self):
         return self.product
--- store/views.py
+++ store/views.py
@@ -1,8 +1,15 @@
+from django.core import paginator
+from django.http.response import HttpResponse
 from django.shortcuts import render, get_object_or_404
+from carts.models import CartItem
+from carts.views import _cart_id, cart
 
 from category.models import Category
 from .models import Product
 from category.models import Category
+from django.db.models import Q
+
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 # Create your views here.
 
@@ -15,13 +22,23 @@ def store(request, category_slug=None):
         categories = get_object_or_404(Category, slug=category_slug)
         products = Product.objects.filter(
             category=categories, is_available=True)
+        # we take only 5 products from the all teh products retrieved above
+        paginator = Paginator(products, 6)
+        page = request.GET.get('page')
+        # here we save the 6 products to use on each webpage instead of the whole products.
+        pages_products = paginator.get_page(page)
         product_count = products.count()
     else:
-        products = Product.objects.all().filter(is_available=True)
+        products = Product.objects.all().filter(is_available=True).order_by('id')
+        # we take only 5 products from the all teh products retrieved above
+        paginator = Paginator(products, 3)
+        page = request.GET.get('page')
+        # here we save the 6 products to use on each webpage instead of the whole products.
+        pages_products = paginator.get_page(page)
         product_count = products.count()
 
     context = {
-        'products': products,
+        'products': pages_products,
         'product_count': product_count,
     }
     return render(request, 'store/store.html', context)
@@ -31,10 +48,31 @@ def product_detail(request, category_slug, product_slug):
     try:
         single_product = Product.objects.get(
             category__slug=category_slug, slug=product_slug)
+        in_cart = CartItem.objects.filter(
+            cart__cart_id=_cart_id(request), product=single_product).exists()
     except Exception as e:
         raise e
 
     context = {
-        "single_product": single_product
+        "single_product": single_product,
+        'in_cart': in_cart
     }
     return render(request, 'store/product_detail.html', context)
+
+
+def search(request):
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        # if keyword:
+        products = Product.objects.order_by('-created_date').filter(
+            Q(description__icontains=keyword) | Q(product_name__icontains=keyword))
+        product_count = products.count()
+        if not keyword:
+            products = ""
+            product_count = 0
+
+    context = {
+        'products': products,
+        'product_count': product_count,
+    }
+    return render(request, 'store/store.html', context)
