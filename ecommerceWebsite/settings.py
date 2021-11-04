9e5440d99adbd342aa9da240053cb2987d5654dc
Mahmoud Gamal <40371141+mahmoudgamal1988@users.noreply.github.com>
2021-11-04 13:38:47 +0200

    Update settings.py

--- ecommerceWebsite/settings.py
+++ ecommerceWebsite/settings.py
@@ -152,5 +152,5 @@ MESSAGE_TAGS = {
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'mahmoudgamal19@gmail.com'
-EMAIL_HOST_PASSWORD = '^%$#@!123456som'
+EMAIL_HOST_PASSWORD = 'Password_Goes_Here'
 EMAIL_USE_TLS = True
